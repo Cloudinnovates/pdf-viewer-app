fa6ceb7d17f88e2c5cb457c6037944e31f991af1
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-14 18:46:42 +0200

    feat: pdf-viewer module added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { HomePage } from './home.page';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    PdfViewerModule,
     RouterModule.forChild([
       {
         path: '',
