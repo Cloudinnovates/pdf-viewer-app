6f2c205c68af866e4102b8ec923f8d8aef8c0495
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-14 19:37:25 +0200

    feat: hammer.js & gesture added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,15 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import { PdfService } from '../pdf.service';
 
 @Component({
-	selector: 'app-home',
-	templateUrl: 'home.page.html',
-	styleUrls: ['home.page.scss']
+    selector: 'app-home',
+    templateUrl: 'home.page.html',
+    styleUrls: ['home.page.scss']
 })
 export class HomePage implements OnInit {
-	title = 'loading...';
-	constructor(private pdfService: PdfService) { }
+    title = 'loading...';
+    constructor(private pdfService: PdfService) { }
 
-	ngOnInit() {
-		this.title = this.pdfService.pdfName;
-	}
+    ngOnInit() {
+        this.title = this.pdfService.pdfName;
+    }
+
+    onSwipeRight(event) {
+        console.log('swipe right');
+    }
+
+    onSwipeLeft(event) {
+        console.log('swipe left');
+    }
 }
