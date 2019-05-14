de08cd2b3db56fa25330f4948d2867e998bce7b6
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-14 19:24:34 +0200

    feat: page title bind

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PdfService } from '../pdf.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+	selector: 'app-home',
+	templateUrl: 'home.page.html',
+	styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+	title = 'loading...';
+	constructor(private pdfService: PdfService) { }
 
+	ngOnInit() {
+		this.title = this.pdfService.pdfName;
+	}
 }
--- src/app/pdf.service.ts
+++ src/app/pdf.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import pdfInfo from '../assets/data/content.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PdfService {
+  public pdfName = 'loading...';
+  constructor() {
+    this.pdfName = pdfInfo.name;
+  }
 
-  constructor() { }
+
 }
