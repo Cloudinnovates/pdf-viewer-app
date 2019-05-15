aa3a80fc883c877a91bd02baa8763f5381be1884
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-15 13:23:19 +0200

    feat: Added animation to content-menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
@@ -19,6 +20,7 @@ import { ContentMenuComponent } from './content-menu/content-menu.component';
   entryComponents: [HomePage],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(),
     IonicStorageModule.forRoot(),
     AppRoutingModule,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,19 @@
 import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 import { PdfService, IPdfPage } from '../pdf.service';
 import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
-    styleUrls: ['home.page.scss']
+    styleUrls: ['home.page.scss'],
+    animations: [
+        trigger('contentMenu', [
+            state('show', style({ transform: 'translateX(0)', opacity: 1 })),
+            state('hide', style({ transform: 'translateX(100%)', opacity: 0 })),
+            transition('* => *', animate(500))
+        ])
+    ]
 })
 export class HomePage implements OnInit, OnDestroy {
     @ViewChild('pdf_viewer') pdf_viewer;
