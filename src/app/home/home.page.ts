686280c00f9140911c5776d1291be170fda31b81
ahmet s <ahmetsametoglu.fr@gmail.com>
2019-05-15 10:48:31 +0200

    feat: resume from last page when app start.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,35 +1,47 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { PdfService } from '../pdf.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-home',
     templateUrl: 'home.page.html',
     styleUrls: ['home.page.scss']
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
     @ViewChild('pdf_viewer') pdf_viewer;
+    pageSubscription: Subscription;
 
     title = 'loading...';
-    currentPage = 1;
+    currentPage = null;
     pdfViewerHeight = 0;
 
     constructor(private pdfService: PdfService) { }
 
     ngOnInit() {
         this.title = this.pdfService.pdfName;
+        this.pageSubscription = this.pdfService.getCurrentPage().subscribe(page => {
+            if (!!page) {
+                this.currentPage = page;
+                console.log('currentPage:', this.currentPage);
+            }
+        });
     }
 
-    onSwipeRight(event) {
-        this.currentPage = this.pdfService.getBeforePage(this.currentPage);
+    ngOnDestroy() {
+        if (this.pageSubscription) {
+            this.pageSubscription.unsubscribe();
+        }
     }
 
-    onSwipeLeft(event) {
-        this.currentPage = this.pdfService.getNextPage(this.currentPage);
+    onSwipeRight() {
+        this.pdfService.beforePage(this.currentPage);
+    }
+
+    onSwipeLeft() {
+        this.pdfService.nextPage(this.currentPage);
     }
 
     pageRendered() {
         this.pdfViewerHeight = this.pdf_viewer.element.nativeElement.scrollHeight;
-        console.log(this.pdfViewerHeight);
-
     }
 }
--- src/app/pdf.service.ts
+++ src/app/pdf.service.ts
@@ -1,20 +1,44 @@
 import { Injectable } from '@angular/core';
 import pdfInfo from '../assets/data/content.json';
-
+import { BehaviorSubject } from 'rxjs';
+import { Storage } from '@ionic/storage';
 @Injectable({
   providedIn: 'root'
 })
 export class PdfService {
+  private firstPage = 1;
+  private currentPage: BehaviorSubject<number> = new BehaviorSubject(null);
   public pdfName = 'loading...';
-  constructor() {
+  constructor(private storage: Storage) {
     this.pdfName = pdfInfo.name;
+    this.firstPage = 1 + pdfInfo.firstPageOffset;
+    console.log(this.firstPage, pdfInfo.firstPageOffset);
+
+    this.storage.get('lastPage').then((val) => {
+      if (!!val) {
+        this.currentPage.next(val);
+      } else {
+        this.currentPage.next(this.firstPage);
+      }
+    });
   }
 
-  getBeforePage(currentPage: number): number {
-    return currentPage === 1 ? 1 : currentPage - 1;
+  beforePage(currentPage: number) {
+    const page = currentPage === this.firstPage ? this.firstPage : currentPage - 1;
+    this.setCurrentPage(page);
   }
-  getNextPage(currentPage: number): number {
-    return currentPage === pdfInfo.pageCount ? pdfInfo.pageCount : currentPage + 1;
+
+  nextPage(currentPage: number) {
+    const page = currentPage === pdfInfo.pageCount ? pdfInfo.pageCount : currentPage + 1;
+    this.setCurrentPage(page);
+  }
+
+  setCurrentPage(v: number) {
+    this.currentPage.next(v);
+    this.storage.set('lastPage', v);
+  }
+  getCurrentPage() {
+    return this.currentPage.asObservable();
   }
 
 }
